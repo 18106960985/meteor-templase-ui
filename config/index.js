704f49e11ec768f9d00369e002dcfaff702a0bb8
yebinghuan <747718944@qq.com>
2018-04-30 14:30:59 +0800

    让人吐血的下载问题啊啊啊啊

--- config/index.js
+++ config/index.js
@@ -57,10 +57,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../meteor/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../meteor'),
     assetsSubDirectory: 'static',
 
     // you can set by youself according to actual condition
